b6297fa2e2f213a0f60f75ba0cf3dd8c12b8b46c
bilalhundal <hundalbilal@gmail.com>
2024-10-11 11:40:39 +0100

    Initial commit

---
+++ src/main/java/com/battleship/models/DataDao.java
@@ -0,0 +1,27 @@
+package com.battleship.models;
+
+public class DataDao {
+	int hits=0;
+	int missed=0;
+	String ship;
+	public int getHits() {
+		return hits;
+	}
+	public void setHits(int hits) {
+		this.hits = hits;
+	}
+	public int getMissed() {
+		return missed;
+	}
+	public void setMissed(int missed) {
+		this.missed = missed;
+	}
+	public String getShip() {
+		return ship;
+	}
+	public void setShip(String ship) {
+		this.ship = ship;
+	}
+	
+
+}
